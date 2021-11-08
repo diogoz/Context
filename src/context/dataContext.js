6e86a12b4820feb8b1d894c243d161fcf6cd8084
Diogo Sousa <dhesousa77@gmail.com>
2021-11-08 15:14:50 -0300

    useContext e useReducer funcionando juntos

--- src/context/dataContext.js
+++ src/context/dataContext.js
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, {useReducer} from 'react'
+
+let initialState = {
+    value: 0,
+    showMessage: false
+}
+
+const reducer = (state, action) => {
+    switch(action.type){
+        case "plus":
+            return {... state, value: state.value + action.payload};
+        case "toggle":
+            return {... state, showMessage: true};
+        default:
+            return state
+    }
+}
+
+// action = {type: "", payload: 0}
 
 export const Context = React.createContext()
 export const Provider = ({children}) => {
-    return <Context.Provider value={500}>{children}</Context.Provider>
+    const [state, dispatch] = useReducer(reducer, initialState)
+    return <Context.Provider value={{state, dispatch}}>{children}</Context.Provider>
 }
 
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,16 +1,32 @@
 import React, {useContext} from 'react'
-import { View, Text } from 'react-native'
-import Teste from '../components/Teste';
+import { View, Text, StyleSheet, Button } from 'react-native'
 import { Provider, Context } from '../context/dataContext';
 const Home = () => {
-    const data = useContext(Context);
+    const actualState = useContext(Context);
     return (
-        <View>
-            <Text>Home</Text>
-            <Text>{data}</Text>
-            <Teste />
+        <View style={styles.container}>
+            <Text style={styles.text}>{actualState.state.value}</Text>
+           <Button title="Aumentar" 
+                onPress={() => actualState.dispatch({type: "plus", payload: 1})}
+           />
+           {actualState.state.showMessage ? <Text>Mensagem que não pode ser mostrada</Text> : null}
+           <Button title="Mostrar Mensagem" 
+                onPress={() => actualState.dispatch({type: "toggle", payload: true})}
+            />
+            <Button title="Ocultar  Mensagem" 
+                onPress={() => actualState.dispatch({type: "toggle", payload: false})}
+            />
         </View>
     )
-}
-
+};
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    text: {
+        fontSize: 30,
+    },
+});
 export default Home
